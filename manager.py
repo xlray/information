49905c511d3a48debdb0ff6212c41e61085569f6
ray <644133@qq.com>
2018-06-11 20:42:20 +0800

    蓝图目录创建，视图函数抽取

--- manager.py
+++ manager.py
@@ -12,16 +12,5 @@ manager = Manager(app)
 Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
-
-@app.route('/',methods=['GET','POST'])
-def index():
-    # session['name'] = 'zhangsan'
-
-    logging.debug("调试信息AAAA")
-    logging.info("详细信息AAAA")
-    logging.error("错误信息AAAAA")
-    current_app.logger.debug("调试信息BBBB")
-    current_app.logger.error("错误信息BBBB")
-    return "hello world!"
 if __name__ == '__main__':
     manager.run()
